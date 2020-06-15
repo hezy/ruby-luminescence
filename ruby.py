84977e051a3390a72074f8fc35898dac92f8168c
Hezy Amiel <hezy.amiel@gmail.com>
2020-06-15 15:32:34 +0300

    modified:   ruby.py

--- ruby.py
+++ ruby.py
@@ -12,23 +12,31 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 # read data from csv file
-data = read_csv('6.6GPa-a.csv', skiprows=None, header=None, sep='\t', lineterminator='\n', names=["lambda","intensity"])
-x = data.iloc[:,0]
-y = data.iloc[:,1]
+data = read_csv('6.6GPa-a.csv', skiprows=None, header=None, sep='\t',
+                lineterminator='\n', names=["lambda", "intensity"])
+x = data.iloc[:, 0]
+y = data.iloc[:, 1]
+
 
 def lorentz(x, gamma):
-    return gamma / np.pi / (np.square(x) + np.square(gamma)) 
+    return gamma / np.pi / (np.square(x) + np.square(gamma))
+
 
 def gauss(x, sigma):
     return (np.exp(-np.square(x)/2/np.square(sigma)))/(sigma*np.sqrt(2*np.pi))
 
+
 def voigt(x, sigma, gamma):
-    return np.real(wofz((x + 1j*gamma)/(sigma * np.sqrt(2)))) / (sigma * np.sqrt(2*np.pi))
+    return np.real(wofz((x + 1j*gamma)/(sigma * np.sqrt(2)))) /(sigma * np.sqrt(2*np.pi))
+
 
 def ruby(x, x1, s1, g1, a1, x2, s2, g2, a2, b):
-    return b + a1*voigt((x-x1),s1,g1) + a2*voigt((x-x2),s2,g2)
+    return b + a1*voigt((x-x1), s1, g1) + a2*voigt((x-x2), s2, g2)
 
-popt, pocv = curve_fit(ruby, x, y, p0=(692.2, 0.178, 0.1, 2689.0, 693.7, 0.247, 0.1, 6058.0, 90.4))
+
+popt, pocv = curve_fit(ruby, x, y, p0=(692.2, 0.178, 0.1, 2689.0,
+                                       693.7, 0.247, 0.1, 6058.0,
+                                       90.4))
 print(popt)
 print(pocv)
 

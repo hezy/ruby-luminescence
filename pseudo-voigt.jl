473d61bd85989c2fbfcd6f2761e334ad356ea08e
Hezy Amiel <hezy.amiel@gmail.com>
2024-09-12 18:27:11 +0300

    formmating

--- pseudo-voigt.jl
+++ pseudo-voigt.jl
@@ -4,31 +4,31 @@
 using Plots
 using SpecialFunctions
 
-x = range(start=-3, stop=3, step=0.01)
+x = range(start = -3, stop = 3, step = 0.01)
 
 function Gaussian(x, fwhm)
-	#g1 = 2.0 * √(log(2)/π)
-	#g2 = 4.0 * log(2)
+    #g1 = 2.0 * √(log(2)/π)
+    #g2 = 4.0 * log(2)
     #return @. (g1 / fwhm) * exp((-g2 * x^2) / (fwhm^2)) 
-    σ = fwhm/(2√(2log(2)))
-    return @. 1/√(2π)/σ * exp(-x^2/2σ^2)
+    σ = fwhm / (2√(2log(2)))
+    return @. 1 / √(2π) / σ * exp(-x^2 / 2σ^2)
 end
 
 function Lorentzian(x, fwhm)
-	#l1 = 2/π
-	#l2 = 4.0
+    #l1 = 2/π
+    #l2 = 4.0
     #return @. (l1 / fwhm) / ((1 + l2 * x^2) / fwhm^2 ) 
     γ = fwhm / 2
-    return @. (γ/pi) / (x^2 + γ^2)
+    return @. (γ / pi) / (x^2 + γ^2)
 end
 
 function pseudo_Voigt(x, fwhm, n)
-	return n * Lorentzian(x, fwhm) + (1 - n) * Gaussian(x, fwhm)
+    return n * Lorentzian(x, fwhm) + (1 - n) * Gaussian(x, fwhm)
 end
 
 function Voigt(x, fwhm_L, fwhm_G)
-    γ = fwhm_L/2
-    σ = fwhm_G/(2√(2log(2)))
+    γ = fwhm_L / 2
+    σ = fwhm_G / (2√(2log(2)))
     z = @. -im * (x + im * γ) / (√2 * σ)
     return @. real(erfcx(z)) / (√(2pi) * σ)
 end
@@ -39,7 +39,7 @@ y2 = Gaussian(x, 1)
 y3 = pseudo_Voigt(x, 1, 0.5)
 y4 = Voigt(x, 0.45, 0.72)
 
-p = plot(x,[y1 y2 y3 y4], label=["Lorentzian" "Gaussian" "Pseudo Voigt" "Voigt"])
+p = plot(x, [y1 y2 y3 y4], label = ["Lorentzian" "Gaussian" "Pseudo Voigt" "Voigt"])
 #p = plot(x,[y1 y2 y3], label=["Lorentzian" "Gaussian" "Pseudo Voigt"])
 title!("peak functions")
 xlabel!(raw"x")
--- ruby_fit.jl
+++ ruby_fit.jl
@@ -19,7 +19,7 @@ function peak(λ, λ₀, w, n, A)
 end
 
 function background(λ, b₀, b₁, b₂)
-    return @. b₀ + b₁*λ + b₂*λ^2
+    return @. b₀ + b₁ * λ + b₂ * λ^2
 end
 
 function ruby(λ, p)
@@ -37,18 +37,18 @@ function pressure(λ)
     A = 1904.0
     B = 7.665
     λ₀ = 693.29427
-    return A/B * (((λ-λ₀)/λ₀ + 1)^B - 1)
+    return A / B * (((λ - λ₀) / λ₀ + 1)^B - 1)
 end
 
 df = DataFrame(CSV.File("6.6GPa-b.csv"))
-λ = df[!,1];
-y = df[!,2];
+λ = df[!, 1];
+y = df[!, 2];
 
 max_intensity = findmax(df[!, 2])
 guess_a₁ = max_intensity[1]
 guess_a₂ = 1.6 * guess_a₁
 max_position = max_intensity[2]
-guess_λ₂ = df[max_position[],1]
+guess_λ₂ = df[max_position[], 1]
 guess_λ₁ = guess_λ₂ - 1.58
 
 p0 = [guess_λ₁, 2, 0.5, guess_a₁, guess_λ₂, 2, 0.5, guess_a₂, 0, 0, 0]
@@ -56,7 +56,7 @@ p0 = [guess_λ₁, 2, 0.5, guess_a₁, guess_λ₂, 2, 0.5, guess_a₂, 0, 0, 0]
 fit = curve_fit(ruby, λ, y, p0)
 param = fit.param
 
-scatter(λ , y)
+scatter(λ, y)
 plot!(λ, ruby(λ, param))
 
-p = pressure(param[5]) 
+p = pressure(param[5])
